59d9a87308613682a3a517e4cdd5920f2bb6f491
MinJoon Lee <leedazare@gmail.com>
2021-05-20 17:38:22 +0900

    포스트 상세 페이지에 카테고리 기능 추가하기

--- blog/tests.py
+++ blog/tests.py
@@ -53,12 +53,12 @@ class TestView(TestCase):
         self.assertEqual(about_me_btn.attrs['href'], '/about_me/')
 
 
-        def category_card_test(self, soup):
-            categories_card = soup.find('div', id='categories-card')
-            self.assertIn('Categories', categories_card.text)
-            self.assertIn(f'{self.category_programming.name} ({self.category_programming.post_set.count()})', categories_card.text)
-            self.assertIn(f'{self.category_music.name} ({self.category_music.post_set.count()})', categories_card.text)
-            self.assertIn(f'미분류 (1)', categories_card.text)
+    def category_card_test(self, soup):
+        categories_card = soup.find('div', id='categories-card')
+        self.assertIn('Categories', categories_card.text)
+        self.assertIn(f'{self.category_programming.name} ({self.category_programming.post_set.count()})', categories_card.text)
+        self.assertIn(f'{self.category_music.name} ({self.category_music.post_set.count()})', categories_card.text)
+        self.assertIn(f'미분류 (1)', categories_card.text)
 
 
 
@@ -108,34 +108,30 @@ class TestView(TestCase):
 
 
     def test_post_detail(self):
-        # 1.1. 포스트가 하나 있다.
-        post_001 = Post.objects.create(
-            title='첫 번째 포스트입니다.',
-            content='Hello World. we are the world.',
-            author=self.user_trump
-        )
 
         # 1.2. 그 포스트의 url은 '/blog/1/' 이다.
-        self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
+        self.assertEqual(self.post_001.get_absolute_url(), '/blog/1/')
 
         # 2. 첫 번째 포스트의 상세 페이지 테스트
         # 2.1. 첫 번째 포스트의 url로 접근하면 정상적으로 작동한다(status code: 200).
-        response = self.client.get(post_001.get_absolute_url())
+        response = self.client.get(self.post_001.get_absolute_url())
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
 
         self.navbar_test(soup)
+        self.category_card_test(soup)
 
         # 2.3. 첫 번째 포스트의 제목이 웹 브라우저 탭 타이틀에 들어 있다.
-        self.assertIn(post_001.title, soup.title.text)
+        self.assertIn(self.post_001.title, soup.title.text)
 
         # 2.4. 첫 번째 포스트의 제목이 포스트 영역에 있다.
         main_area = soup.find('div', id='main-area')
         post_area = main_area.find('div', id='post-area')
-        self.assertIn(post_001.title, post_area.text)
+        self.assertIn(self.post_001.title, post_area.text)
+        self.assertIn(self.category_programming.name, post_area.text)
 
         # 2.5. 첫 번쨰 포스트의 작성자(author)가 포스트 영역에 있다(아직 구현할 수 없음).
         self.assertIn(self.user_trump.username.upper(), post_area.text)
 
         # 2.6. 첫 번째 포스트의 내용(content)이 포스트 영역에 있다.
-        self.assertIn(post_001.content, post_area.text)
+        self.assertIn(self.post_001.content, post_area.text)
