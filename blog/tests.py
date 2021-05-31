b51f0c81fa010c6e9f3c03874e31d182a8af0204
MinJoon Lee <leedazare@gmail.com>
2021-05-31 22:48:17 +0900

    포스트 작성 페이지 접근 권한 설정하기

--- blog/tests.py
+++ blog/tests.py
@@ -9,6 +9,8 @@ class TestView(TestCase):
         self.user_obama = User.objects.create_user(username='obama', password='somepassword')
         self.category_programming = Category.objects.create(name='programming', slug='programming')
         self.category_music = Category.objects.create(name='music', slug='music')
+        self.user_obama.is_staff = True
+        self.user_obama.save()
 
         self.tag_python_kor = Tag.objects.create(name='파이썬 공부', slug='파이썬-공부')
         self.tag_python = Tag.objects.create(name='python', slug='python')
@@ -161,11 +163,13 @@ class TestView(TestCase):
         response = self.client.get('/blog/create_post/')
         self.assertNotEqual(response.status_code, 200)
 
-
-        #로그인을 한다.
+        # staff가 아닌 trump가 로그인을 한다.
         self.client.login(username='trump', password='somepassword')
+        response = self.client.get('/blog/create_post/')
+        self.assertNotEqual(response.status_code, 200)
 
-
+        # staff인 obama로 로그인한다.
+        self.client.login(username='obama', password='somepassword')
         response = self.client.get('/blog/create_post/')
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
@@ -183,6 +187,6 @@ class TestView(TestCase):
         )
         last_post = Post.objects.last()
         self.assertEqual(last_post.title, "Post Form 만들기")
-        self.assertEqual(last_post.author.username, 'trump')
+        self.assertEqual(last_post.author.username, 'obama')
 
 
