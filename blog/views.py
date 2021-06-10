7d3deae8f2325ae1602c6cb6a97d505029f8c6db
MinJoon Lee <leedazare@gmail.com>
2021-06-10 10:34:04 +0900

    댓글 삭제 기능 추가하기

--- blog/views.py
+++ blog/views.py
@@ -163,7 +163,14 @@ def new_comment(request, pk):
         return PermissionDenied
 
 
-
+def delete_comment(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post = comment.post
+    if request.user.is_authenticated and request.user == comment.author:
+        comment.delete()
+        return redirect(post.get_absolute_url())
+    else:
+        raise PermissionDenied
 
 
 
