574bb6e084468fe14e9621bc2c009741b0306cc4
MinJoon Lee <leedazare@gmail.com>
2021-06-14 23:58:17 +0900

    도커에 장고만 담기

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -13,10 +13,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1d&)(7dvg@6)es^kag(^*%u$+kb)w!ef5xeltuel!ltlwqw*41'
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-1d&)(7dvg@6)es^kag(^*%u$+kb)w!ef5xeltuel!ltlwqw*41')
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = []
+DEBUG = int(os.environ.get('DEBUG', 1))
+
+if os.environ.get('DJANGO_ALLOWED_HOSTS'):
+    ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
+else:
+    ALLOWED_HOSTS = []
+
 
 
 # Application definition
