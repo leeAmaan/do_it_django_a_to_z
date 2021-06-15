7670f24f465d8db124927686ef288aee325b45aa
MinJoon Lee <leedazare@gmail.com>
2021-06-15 10:19:21 +0900

    도커에 gunicorn 적용하기

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -114,6 +114,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/templates/'
+STATIC_ROOT = os.path.join(BASE_DIR, '_static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
