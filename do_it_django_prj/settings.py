c45b984aea6c867982cee8250f9c84eb006c5acf
MinJoon Lee <leedazare@gmail.com>
2021-06-16 14:16:47 +0900

    Nginx 적용하기

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -113,7 +113,7 @@ USE_L10N = True
 USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_URL = '/templates/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '_static')
 
 MEDIA_URL = '/media/'
