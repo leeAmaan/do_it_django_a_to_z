b835bd26b5df661243556bd599707be982d5f605
MinJoon Lee <leedazare@gmail.com>
2021-06-20 17:02:08 +0900

    letsencrpyt 적용하기2

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,7 +9,7 @@ domains=(leedazare.com www.leedazare.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="leedazare@gmail.com"
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
