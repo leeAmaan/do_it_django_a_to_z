a73169049d229119ef632972d700e072848102ed
MinJoon Lee <leedazare@gmail.com>
2021-06-22 16:49:03 +0900

    letsencrypt 적용하기

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -8,7 +8,7 @@ fi
 domains=(leedazare.com www.leedazare.com)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="leedazare@gmail.com"
+email="leedazare@gmail.com" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
